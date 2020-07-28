20e52f10d4cfc27afdc2a21f2764ee8d09cb8936
Alexsander <kas-cor@users.noreply.github.com>
2020-07-28 10:52:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git clone https://github.com/kas-cor/.vimrc.git ~/.vim
+cd ~/.vim
 mkdir tmp
 mkdir undo
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
